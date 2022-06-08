173534431d4fc4faefb005efe92e54a33fa42033
Shahab <azimishahab@gmail.com>
2022-06-08 18:31:40 +0430

    test added

--- app/src/androidTest/java/ir/shahabazimi/mydraft/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ir/shahabazimi/mydraft/ExampleInstrumentedTest.kt
@@ -2,23 +2,22 @@ package ir.shahabazimi.mydraft
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import ir.shahabazimi.mydraft.security.MySp
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
+    fun checkEncryptDecrypt() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ir.shahabazimi.mydraft", appContext.packageName)
+        val target = "Test Sentence"
+        val sp = MySp(appContext)
+        sp.save(target)
+        assertEquals(sp.load(), target)
     }
 }
